ea887d0d4e6e55000c7c7ed9904820b8cfe942b8
Ethan Cannelongo <ethancannelongo@gmail.com>
2024-11-12 14:24:24 -0500

    Added suspense fallback on all pages

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,11 @@
+import { Suspense } from 'react';
+
 export default function Custom404() {
   return (
-    <h1 style={{ textAlign: 'center' }}>
-      404 - Sorry this page is still in development
-    </h1>
+    <Suspense fallback={<div>Loading...</div>}>
+      <h1 style={{ textAlign: 'center' }}>
+        404 - Sorry this page is still in development
+      </h1>
+    </Suspense>
   );
 }
