bbc90c81d59f2006aa1098bfced8cce5109be86b
Ethan Cannelongo <ethancannelongo@gmail.com>
2024-11-12 14:53:10 -0500

    Update in the workflow file

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   basePath: '/sharp-innovations-assessment',
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 
   eslint: {
     ignoreDuringBuilds: true,
